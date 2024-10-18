69d6aef9243e6e6e3d011e63decd0cb4974800d5
MaxWhite2024 <mjwhite@scu.edu>
2024-10-18 11:52:59 -0400

    start if shifting terrain

--- 2D_PCG_Terrain/Assets/Scripts/Player_Controller.cs
+++ 2D_PCG_Terrain/Assets/Scripts/Player_Controller.cs
@@ -8,22 +8,20 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float jumpForce = 1f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("Jump");
+            //rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.velocity += new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         //Debug.Log(Input.GetAxisRaw("Horizontal"));
-        rb.AddForce(Vector2.right * Input.GetAxisRaw("Horizontal") * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Force);
-
-        if(Input.GetKeyDown("space"))
-        {
-            Debug.Log("Jump");
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        //rb.AddForce(Vector2.right * Input.GetAxisRaw("Horizontal") * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Force);
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.fixedDeltaTime, rb.velocity.y);
     }
 }
--- 2D_PCG_Terrain/Assets/Scripts/TwoDimTerrainGen.cs
+++ 2D_PCG_Terrain/Assets/Scripts/TwoDimTerrainGen.cs
@@ -1,6 +1,9 @@
+using System;
 using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -9,6 +12,7 @@ public class TwoDimTerrainGen : MonoBehaviour
     [Header("Generation vars")]
     [SerializeField] private int width;
     [SerializeField] private int height;
+    [SerializeField, Range(0, 100)] private int horizonHeight;
     [SerializeField] private float smoothness;
     [SerializeField] private float seed;
 
@@ -25,7 +29,7 @@ public class TwoDimTerrainGen : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.G))
         {
             Generate();
         }
@@ -36,13 +40,18 @@ public class TwoDimTerrainGen : MonoBehaviour
         //Clear all pre-existing ground tiles
         groundTileMap.ClearAllTiles();
 
-        //
+        //Create an array to manipulate later
         map = GenerateArray(width, height, true);
 
-        //
-        map = GenerateTerrain(map);
+        //Generate horizon from created array
+        map = GenerateHorizon(map);
 
-        //
+        //Generate Terrain by shifting horizon line from created array with horizon
+        map = ShiftTerrainHorizontal(map);
+
+        //map = GenerateTerrain(map);
+
+        //Render a tilemap using the array
         RenderMap(map, groundTileMap, groundTile);
     }
 
@@ -62,6 +71,41 @@ public class TwoDimTerrainGen : MonoBehaviour
                 //else if empty is false, set map[x,y] to 1
                 map[x, y] = (empty) ? 0 : 1;
             }
+        }
+
+        //return map
+        return map;
+    }
+
+    private int[,] GenerateHorizon(int[,] map)
+    {
+        //get and store width and height of map
+        int width = map.GetLength(0);
+        int height = map.GetLength(1);
+
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                //if current y is less then or equal to horizonHeight, set map[x,y] to 1
+                //else if y is greater than horizonHeight, set map[x,y] to 0
+                map[x, y] = (y <= horizonHeight) ? 1 : 0;
+            }
+        }
+
+        //return map
+        return map;
+    }
+
+    private int[,] ShiftTerrainHorizontal(int[,] map)
+    {
+        //get and store width and height of map
+        int width = map.GetLength(0);
+        int height = map.GetLength(1);
+
+        for(int x = 0; x < width; x++)
+        {
+
         }
 
         //return map
@@ -109,5 +153,49 @@ public class TwoDimTerrainGen : MonoBehaviour
                 }
             }
         }
+    }
+
+    private int[] ShiftHorizontal(int[] column, int amount)
+    {
+        int height = column.Length;
+
+        //int[] newColumn = column;
+
+        //amount is positive,...
+        //if(amount > 0)
+        //{
+        //    //iterate downwards through newColumn and 
+        //    for (int y = height; y > 0; y--)
+        //    {
+        //        if()
+        //        column[y] = 
+        //    }
+        //}
+        ////else if amount is negative,...
+        //else if(amount < 0)
+        //{
+        //    for (int y = 0; y < height; y++)
+        //    {
+
+        //    }
+        //}
+
+        //return shifted column
+        return column;
+    }
+
+    //helper functions courtesy of Alex Podles on https://stackoverflow.com/questions/27427527/how-to-get-a-complete-row-or-column-from-2d-array-in-c-sharp
+    public int[] GetColumn(int[,] matrix, int columnNumber)
+    {
+        return Enumerable.Range(0, matrix.GetLength(0))
+                .Select(x => matrix[x, columnNumber])
+                .ToArray();
+    }
+
+    public int[] GetRow(int[,] matrix, int rowNumber)
+    {
+        return Enumerable.Range(0, matrix.GetLength(1))
+                .Select(x => matrix[rowNumber, x])
+                .ToArray();
     }
 }
